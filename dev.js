d0920e7c10514377a16c69c0af8030a8f49feda6
muhammad zubair <labim34@gmail.com>
2022-08-18 16:59:33 +0200

    🎉 init 🎉

---
+++ dev.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+const nodemon = require('nodemon');
+const ngrok = require('ngrok');
+
+const TOKEN = process.env.NGROK_TOKEN;
+const PORT = process.env.PORT;
+
+try {
+    ngrok.connect(PORT, {
+        authtoken: TOKEN,
+        region: 'eu',
+    }).then(url => {
+        console.log(`Server is running on ${url}`);
+        nodemon({
+            script: './src/index.js',
+            ext: 'js json',
+            env: {
+                TOKEN,
+                PORT,
+            }
+        }).on('start', () => {
+            console.log('started!');
+        }).on('restart', () => {
+            console.log(`Server is running on ${url}`);
+            console.log('restarted!');
+        }).on('crash', () => {
+            console.log('crashed!');
+        }).on('quit', () => {
+            ngrok.kill().then(() => process.exit(0)).catch(err => console.log(err));
+            console.log('quited!');
+        }).on('exit', () => {
+            console.log('exited!');
+        }).on('error', () => {
+            console.log('error!');
+        });
+    }).catch(err => {
+        console.log(err);
+    });
+} catch (error) {
+    console.error(error);
+}
