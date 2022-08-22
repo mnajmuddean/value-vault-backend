8951736a051922ce089d367222b4012a59f9d4ed
muhammad zubair <labim34@gmail.com>
2022-08-22 04:29:24 +0200

    ➖ Switched from body parser to express json ➖

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!, process.pid: ' + process.pid);
