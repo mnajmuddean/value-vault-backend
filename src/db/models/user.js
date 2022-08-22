f5ff3ea4307e877175096cfeba083a3977f1dc88
mzubair481 <labim34@gmail.com>
2022-08-22 23:13:57 +0200

    🗃️ Added Database Support 🗃️

---
+++ src/db/models/user.js
@@ -0,0 +1,55 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasOne(models.Token, {
+        foreignKey: 'userId',
+        as: 'token',
+      });
+    }
+  }
+  User.init({
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    emailVerified: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  }, {
+    sequelize,
+    modelName: 'User',
+    // exclude password
+    exclude: ['password'],
+    // include password with a custom getter
+    getterMethods: {
+      password() {
+        return this.getDataValue('password');
+      },
+    },
+  });
+  return User;
+};
