78c7e1fa70ce180e2e35b09b0f040629891374a7
mzubair481 <labim34@gmail.com>
2022-08-29 15:01:20 +0200

    🐛 Fixed Database connection error 🐛

--- index.js
+++ index.js
@@ -24,23 +24,22 @@ if (process.env.NODE_ENV === 'development') {
     });
 } else if (cluster.isMaster) {
   console.log('This is the master process:', process.pid);
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+  sequelize
+    .sync({ force: true })
+    .then(() => {
+      for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   cluster.on('exit', (worker, code, signal) => {
     console.log(`Worker ${worker.process.pid} died`);
     cluster.fork();
   });
 } else {
   try {
-    sequelize
-      .authenticate()
-      .then(() => {
-        console.log('Connection has been established successfully.');
-      })
-      .catch((err) => {
-        console.error('Unable to connect to the database:', err);
-      });
     app.listen(port, () => {
       console.log(`App listening on port ${port}! Process ID: ${process.pid}`);
     });
