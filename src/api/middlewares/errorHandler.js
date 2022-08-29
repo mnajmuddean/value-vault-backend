18b58bf4b48b00300d2bca5e75ccfb72ad18e995
mzubair481 <labim34@gmail.com>
2022-08-29 17:47:16 +0200

    ✏️ Fixed a Typo ✏️

--- src/api/middlewares/errorHandler.js
+++ src/api/middlewares/errorHandler.js
@@ -5,7 +5,7 @@ const errorHandler = (err, req, res, next) => {
   if (err instanceof ApiError) {
     res.status(err.statusCode).json({ message: err.message });
   } else {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 };
 
