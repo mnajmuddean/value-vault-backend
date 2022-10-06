47bf190039e64d407616c53f8314aba9e19eb0f2
mzubair481 <labim34@gmail.com>
2022-10-06 17:09:06 +0200

    ➕ Added cors and joi  ➕

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./api/routes');
 const errorHandler = require('./api/middlewares/errorHandler');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
