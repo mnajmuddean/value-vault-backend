2459eaf1f93ec666c16cf32cdfc76ef6870c932b
mzubair481 <labim34@gmail.com>
2022-10-16 20:43:26 +0200

    🛂 Added cutom auth values 🛂

--- src/api/validations/auth.validations.js
+++ src/api/validations/auth.validations.js
@@ -1,23 +1,24 @@
 const joi = require('joi');
+const { username, password, email, refreshToken } = require('./custom.validations');
 
 const register = {
   body: joi.object().keys({
-    username: joi.string().required(),
-    email: joi.string().email().required(),
-    password: joi.string().required(),
+    username: joi.string().required().custom(username),
+    email: joi.string().email().required().custom(email),
+    password: joi.string().required().custom(password),
   }),
 };
 
 const login = {
   body: joi.object().keys({
-    username: joi.string().required(),
-    password: joi.string().required(),
+    username: joi.string().required().custom(username),
+    password: joi.string().required().custom(password),
   }),
 };
 
 const refreshTokens = {
   body: joi.object().keys({
-    refreshToken: joi.string().required(),
+    refreshToken: joi.string().required().custom(refreshToken),
   }),
 };
 
