fde6e7752f5c13ab4bc6cf64d21c3576386d6ca9
mzubair481 <labim34@gmail.com>
2022-10-30 20:14:53 +0100

    🥅 Updated Error Handler 🥅

--- src/api/middlewares/errorHandler.js
+++ src/api/middlewares/errorHandler.js
@@ -2,11 +2,22 @@ const ApiError = require('../utils/ApiError');
 
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
-  if (err instanceof ApiError) {
-    res.status(err.statusCode).json({ message: err.message });
-  } else {
-    res.status(500).json({ message: 'Internal Server Error', error: err });
+  let { stack } = err;
+  if (process.env.NODE_ENV === 'production') {
+    stack = undefined;
   }
+  if (err instanceof ApiError) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+      stack,
+    });
+  }
+  return res.status(500).json({
+    status: 'error',
+    message: err.message,
+    stack,
+  });
 };
 
 module.exports = errorHandler;
